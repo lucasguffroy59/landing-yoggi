a48cf60b16c6b06f5849baab51986df391cd07bf
Lucas Guffroy <lucas.guffroy@gmail.com>
2026-07-14 20:14:09 +0200

    added sending of analytics

--- analytics-events.js
+++ analytics-events.js
@@ -1,4 +1,35 @@
 (function () {
+  var BACKEND_EVENTS_URL = "https://yoggi-backend.up.railway.app/api/analytics/events";
+  var LANDING_ANALYTICS_TOKEN = "d9ed57ddfdcea52954c96539f32a6a3edf36b05ce35b140b";
+
+  function getAnonymousId() {
+    var key = "yoggi_anonymous_id";
+    var id = localStorage.getItem(key);
+    if (!id) {
+      id = crypto.randomUUID();
+      localStorage.setItem(key, id);
+    }
+    return id;
+  }
+
+  function sendBackendEvent(eventName, properties) {
+    try {
+      fetch(BACKEND_EVENTS_URL, {
+        method: "POST",
+        keepalive: true,
+        headers: {
+          "Content-Type": "application/json",
+          "X-App-Token": LANDING_ANALYTICS_TOKEN,
+        },
+        body: JSON.stringify({
+          anonymousId: getAnonymousId(),
+          eventName: eventName,
+          properties: properties || {},
+        }),
+      }).catch(function () {});
+    } catch (err) {}
+  }
+
   function fire(eventName) {
     if (typeof gtag === "function") {
       gtag("event", eventName);
@@ -9,6 +40,12 @@
     var pageId = document.body.getAttribute("data-ga-page");
     if (pageId) {
       fire("page_view_" + pageId);
+    }
+
+    var firstVisitKey = "yoggi_landing_first_visit_sent";
+    if (!localStorage.getItem(firstVisitKey)) {
+      sendBackendEvent("landing_first_visit", {});
+      localStorage.setItem(firstVisitKey, "1");
     }
   });
 
@@ -42,9 +79,11 @@
 
     if (href.indexOf("apps.apple.com") !== -1) {
       fire("app_store_click_" + loc);
+      sendBackendEvent("landing_store_click", { platform: "ios", loc: loc });
       reportStoreConversion(e, href);
     } else if (href.indexOf("play.google.com") !== -1) {
       fire("play_store_click_" + loc);
+      sendBackendEvent("landing_store_click", { platform: "android", loc: loc });
       reportStoreConversion(e, href);
     }
   });
