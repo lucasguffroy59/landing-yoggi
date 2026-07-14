27c978c15b7c5a9880a39f2bde248c2bd5ca620a
Lucas Guffroy <lucas.guffroy@gmail.com>
2026-07-14 16:19:17 +0200

    added google tag for ads

--- analytics-events.js
+++ analytics-events.js
@@ -12,6 +12,19 @@
     }
   });
 
+  function reportStoreConversion(e, href) {
+    if (typeof gtag !== "function") return;
+    e.preventDefault();
+    gtag("event", "conversion", {
+      send_to: "AW-18092979969/lt92CP3XuqscEIHus7ND",
+      value: 1.0,
+      currency: "EUR",
+      event_callback: function () {
+        window.location = href;
+      },
+    });
+  }
+
   document.addEventListener("click", function (e) {
     var link = e.target.closest("a[href]");
     if (!link) return;
@@ -21,8 +34,10 @@
 
     if (href.indexOf("apps.apple.com") !== -1) {
       fire("app_store_click_" + loc);
+      reportStoreConversion(e, href);
     } else if (href.indexOf("play.google.com") !== -1) {
       fire("play_store_click_" + loc);
+      reportStoreConversion(e, href);
     }
   });
 })();
